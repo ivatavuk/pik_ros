873a9a96419a3129780e8faf89d9d66fd310b79c
ivatavuk <ivo.vatavuk@fer.hr>
2023-06-01 20:57:57 +0200

    Apply clang-format

--- include/MoveItWrapper.hpp
+++ include/MoveItWrapper.hpp
@@ -9,7 +9,7 @@
 #define MOVEIT_WRAPPER_HPP_
 
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <moveit/robot_model_loader/robot_model_loader.h> 
+#include <moveit/robot_model_loader/robot_model_loader.h>
 
 #include <vector>
 #include <Eigen/Dense>
@@ -20,24 +20,21 @@ typedef moveit::planning_interface::MoveGroupInterfacePtr MoveGroupInterfacePtr;
 class MoveItWrapper
 {
 public:
-  MoveItWrapper(ros::NodeHandle &nh, const std::string &robot_name, 
-                const std::string &joint_model_group_name);
+  MoveItWrapper(ros::NodeHandle& nh, const std::string& robot_name, const std::string& joint_model_group_name);
 
-  void goToJointPosition( const Eigen::VectorXd &q, 
-                          double acc_scaling_factor, 
-                          double vel_scaling_factor) const;
+  void goToJointPosition(const Eigen::VectorXd& q, double acc_scaling_factor, double vel_scaling_factor) const;
 
-  Eigen::MatrixXd getJacobian(const std::string &link_name, const Eigen::VectorXd &q, 
+  Eigen::MatrixXd getJacobian(const std::string& link_name, const Eigen::VectorXd& q,
                               bool use_quaternion_representation = false) const;
 
-  Eigen::VectorXd getFramePose(const std::string &link_name, const Eigen::VectorXd &q) const;
+  Eigen::VectorXd getFramePose(const std::string& link_name, const Eigen::VectorXd& q) const;
 
-  uint32_t getNDof() const;     
+  uint32_t getNDof() const;
 
 private:
   MoveGroupInterfacePtr move_group_;
   robot_model_loader::RobotModelLoaderPtr robot_model_loader_;
-  robot_state::JointModelGroup *joint_model_group_;
+  robot_state::JointModelGroup* joint_model_group_;
   robot_state::RobotStatePtr kinematic_state_;
 
   ros::NodeHandle nh_;
@@ -45,7 +42,7 @@ private:
   std::string joint_model_group_name_;
   std::string robot_model_name_;
 
-  static std::vector<double> eigenVec2StdVec(const Eigen::VectorXd &eig_vec);
-}; 
+  static std::vector<double> eigenVec2StdVec(const Eigen::VectorXd& eig_vec);
+};
 
-#endif //MOVEIT_WRAPPER_HPP_
+#endif  // MOVEIT_WRAPPER_HPP_
--- include/PikRos.hpp
+++ include/PikRos.hpp
@@ -15,18 +15,17 @@
 
 namespace PikRos
 {
-
 /** @enum Ik task type */
 enum IkTaskType
 {
-  FRAME_POSITION, /** < Frame position task, has a value of [x, y, z]^T in meters */
-  FRAME_ORIENTATION, /** < Frame orientation task, has a quaternion value [w, x, y, z]^T */
-  FRAME_POSE, /** < Frame pose task, a combination of FRAME_POSITION and FRAME_ORIENTATION tasks */
+  FRAME_POSITION,     /** < Frame position task, has a value of [x, y, z]^T in meters */
+  FRAME_ORIENTATION,  /** < Frame orientation task, has a quaternion value [w, x, y, z]^T */
+  FRAME_POSE,         /** < Frame pose task, a combination of FRAME_POSITION and FRAME_ORIENTATION tasks */
   FRAME_APPROACH_AXIS /** < Frame approach axis task, has a value of a 3D vector [x, y, z]^T */
 };
 
 /** @enum Approach axis
- * 
+ *
  * Which axis of the target frame is considered its approach axis
  */
 enum ApproachAxis
@@ -41,15 +40,14 @@ struct IkTask
 {
   /**
    * @brief Construct a new Ik Task object
-   * 
+   *
    * @param t_type IkTask type
    * @param t_frame_name MoveIt frame name
    * @param t_desired_value IkTask desired value
    * @param t_approach_axis Which frame axis is considered the approach axis. Used only for the task type FRAME_APPROACH_AXIS
    */
-  IkTask( IkTaskType t_type, const std::string &t_frame_name, 
-          const Eigen::VectorXd &t_desired_value, 
-          ApproachAxis t_approach_axis = Z);
+  IkTask(IkTaskType t_type, const std::string& t_frame_name, const Eigen::VectorXd& t_desired_value,
+         ApproachAxis t_approach_axis = Z);
 
   /** @brief Get the approach axis Vector */
   Eigen::Vector3d getAppAxisVector() const;
@@ -61,20 +59,21 @@ struct IkTask
 };
 
 /** @brief Solver settings */
-struct Settings 
-{ 
-  uint32_t max_iterations = 10000; /**< Maximum number of solve iterations */
-  double  err_norm_threshold = 1e-3; /**< Threshold for the sum of task error norms */ 
+struct Settings
+{
+  uint32_t max_iterations = 10000;  /**< Maximum number of solve iterations */
+  double err_norm_threshold = 1e-3; /**< Threshold for the sum of task error norms */
   double gradient_threshold = 1e-3; /**< Threshold for the sum of task gradients */
-  double polish_gradient_threshold = 1e-2; /**< When sum of task gradients reaches this threshold solution polishing starts */
-  double dq_limit = (double)(10.0 * M_PI / 180.0); /**< dq limit */
+  double polish_gradient_threshold =
+      1e-2; /**< When sum of task gradients reaches this threshold solution polishing starts */
+  double dq_limit = (double)(10.0 * M_PI / 180.0);       /**< dq limit */
   double polish_dq_limit = (double)(3.0 * M_PI / 180.0); /**< dq limit for solution polishing */
-  double lin_err_clamp_magnitude = 0.2; /**< Clamp magnitude for linear tasks */
-  double ang_err_clamp_magnitude = 15.0 * M_PI / 180.0; /**< Clamp magnitude for angular tasks */
+  double lin_err_clamp_magnitude = 0.2;                  /**< Clamp magnitude for linear tasks */
+  double ang_err_clamp_magnitude = 15.0 * M_PI / 180.0;  /**< Clamp magnitude for angular tasks */
   bool use_constrained_opt = true;
-  bool polish_solution = false; 
+  bool polish_solution = false;
   bool debug_mode = false; /**< Move the arm in gazebo for every step with verbose output */
-  double timeout = 2.0; /**< Solver timeout in seconds */
+  double timeout = 2.0;    /**< Solver timeout in seconds */
 };
 
 /** @brief Gradient class */
@@ -84,6 +83,7 @@ public:
   Gradient(double initial_value);
   void updateGradient(double current_value);
   double getGradient() const;
+
 private:
   double current_value_;
   double ex_value_;
@@ -101,73 +101,69 @@ public:
   /**
    * @brief Construct a new Pik object
    * @param nh ROS node handle
-   * @param robot_name Namespace for robot description 
+   * @param robot_name Namespace for robot description
    * @param joint_model_group_name Joint model group name in MoveIt
    * @param settings PikRos::Settings object
    */
-  Pik(ros::NodeHandle &nh, const std::string &robot_name, 
-      const std::string &joint_model_group_name,
-      const Settings &settings = Settings());
+  Pik(ros::NodeHandle& nh, const std::string& robot_name, const std::string& joint_model_group_name,
+      const Settings& settings = Settings());
 
   /**
    * @brief Solves a prioritized inverse kinematics problem
-   * @param ik_tasks Ik tasks std::vector<IkTask> from higher priority to lower  
+   * @param ik_tasks Ik tasks std::vector<IkTask> from higher priority to lower
    * @param q_initial_guess Initial guess for a joint position vector q
    * @return Eigen::VectorXd solution joint positions
    */
-  Eigen::VectorXd solve(const std::vector<IkTask> &ik_tasks, 
-                        const Eigen::VectorXd& q_initial_guess );
+  Eigen::VectorXd solve(const std::vector<IkTask>& ik_tasks, const Eigen::VectorXd& q_initial_guess);
 
   /**
    * @brief Moves the robot to desired joint positions
    * @param q Eigen::VectorXd Joint position vector
    */
-  void goToJointPosition(const Eigen::VectorXd &q) const;
+  void goToJointPosition(const Eigen::VectorXd& q) const;
 
   /**
    * @brief Prints task errors for a given joint position vector
    * @param q Eigen::VectorXd Joint position vector
    * @param ik_tasks std::vector<IkTask> Inverse kinematics tasks;
    */
-  void printErrors(const Eigen::VectorXd &q, const std::vector<IkTask> &ik_tasks) const;
+  void printErrors(const Eigen::VectorXd& q, const std::vector<IkTask>& ik_tasks) const;
 
 private:
   MoveItWrapper moveit_wrapper_;
   Settings settings_;
   const uint32_t n_dof_;
 
-  Eigen::Vector3d getOrientationErrorRPY( Eigen::Quaterniond desired_quat, 
-                                          Eigen::Quaterniond current_quat,
-                                          bool clamp_err = false ) const;
-          
-  bool reachedErrNormThreshold(const Eigen::VectorXd &q, const std::vector<IkTask> &ik_tasks) const;
-  static double getAbsGradientSum(const std::vector<Gradient> &gradients);
-  bool reachedGradientThreshold(const std::vector<Gradient> &gradients) const;
-  
-  Eigen::MatrixXd getTaskJacobian( const Eigen::VectorXd &q, IkTask task ) const; 
-  Eigen::VectorXd solvePtscProblem(const std::vector<PtscEigen::Task> &ptsc_tasks) const;
+  Eigen::Vector3d getOrientationErrorRPY(Eigen::Quaterniond desired_quat, Eigen::Quaterniond current_quat,
+                                         bool clamp_err = false) const;
 
-  Eigen::VectorXd getTaskValue( const Eigen::VectorXd &q, IkTask task ) const; //These functions go to IkTask!
-  Eigen::VectorXd getTaskError( const Eigen::VectorXd &q, IkTask task, bool clamp_err = false ) const; 
+  bool reachedErrNormThreshold(const Eigen::VectorXd& q, const std::vector<IkTask>& ik_tasks) const;
+  static double getAbsGradientSum(const std::vector<Gradient>& gradients);
+  bool reachedGradientThreshold(const std::vector<Gradient>& gradients) const;
 
-  static Eigen::VectorXd clampMagnitude(const Eigen::VectorXd &input_vec, double magnitude);
-  static Eigen::VectorXd limitQ(const Eigen::VectorXd &q);
-  static Eigen::VectorXd limitDq(const Eigen::VectorXd &dq, double dq_limit);
-  static Eigen::Quaterniond vec2quat(const Eigen::VectorXd &q_vec);
+  Eigen::MatrixXd getTaskJacobian(const Eigen::VectorXd& q, IkTask task) const;
+  Eigen::VectorXd solvePtscProblem(const std::vector<PtscEigen::Task>& ptsc_tasks) const;
+
+  Eigen::VectorXd getTaskValue(const Eigen::VectorXd& q, IkTask task) const;  // These functions go to IkTask!
+  Eigen::VectorXd getTaskError(const Eigen::VectorXd& q, IkTask task, bool clamp_err = false) const;
+
+  static Eigen::VectorXd clampMagnitude(const Eigen::VectorXd& input_vec, double magnitude);
+  static Eigen::VectorXd limitQ(const Eigen::VectorXd& q);
+  static Eigen::VectorXd limitDq(const Eigen::VectorXd& dq, double dq_limit);
+  static Eigen::Quaterniond vec2quat(const Eigen::VectorXd& q_vec);
 
   bool reached_polish_solution_threshold_ = false;
 
   static int sign(double a);
 
-  bool reachedStoppingCriteria() const; 
-  void initializeSolver(const std::vector<IkTask> &ik_tasks,
-                        const Eigen::VectorXd& q_initial_guess); 
+  bool reachedStoppingCriteria() const;
+  void initializeSolver(const std::vector<IkTask>& ik_tasks, const Eigen::VectorXd& q_initial_guess);
 
   Eigen::VectorXd current_q_;
   std::vector<PikRos::IkTask> ik_tasks_;
-  double solver_elapsed_time_; //in seconds
+  double solver_elapsed_time_;  // in seconds
   std::vector<Gradient> current_gradients_;
   uint32_t current_ik_iteration_;
 };
-};
-#endif //PIK_ROS_H_
+};      // namespace PikRos
+#endif  // PIK_ROS_H_
